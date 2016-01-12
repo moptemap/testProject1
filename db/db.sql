353fcc8fc88ea02261fb881b49cc24265eed90d9
moptemap <moptemap@tut.by>
2016-01-12 22:53:01 +0300

    added DB directory and db.sql script

---
+++ db/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE classname(
+	id SERIAL NOT NULL PRIMARY KEY,
+	classname CHARACTER VARYING(150) NOT NULL UNIQUE
+);
+
+CREATE TABLE keyword(
+	id SERIAL NOT NULL PRIMARY KEY,
+	binded_class INT REFERENCES classname(id),
+	keyword CHARACTER VARYING(50) NOT NULL UNIQUE
+);
+
+INSERT INTO classname(classname) VALUES
+('ie.globalcom.Bell'),
+('ie.globalcom.Bulb'),
+('ie.globalcom.Microwave');
+
+
+INSERT INTO keyword(keyword, binded_class) VALUES
+('ring', 1),
+('bell', 1),
+('light', 2),
+('microwave', 3);
